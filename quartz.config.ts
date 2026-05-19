93fff5779003a01d3e6641672bc607c83567bca3
Saksham Arora <sakshamarora@Sakshams-MacBook-Air.local>
2026-05-19 19:35:33 +0530

    fix: set baseUrl to production domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "localhost:8080",
+    baseUrl: "saksham10arora-dotcom.github.io",
     ignorePatterns: [
       "private",
       "templates",
