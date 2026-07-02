52a385c1014674197fab1beb827367e5ac9c8647
Saksham Arora <saksham10arora@gmail.com>
2026-07-02 14:23:32 +0530

    config: baseUrl -> blog.saksham.digital

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "saksham10arora-dotcom.github.io",
+    baseUrl: "blog.saksham.digital",
     ignorePatterns: [
       "private",
       "templates",
